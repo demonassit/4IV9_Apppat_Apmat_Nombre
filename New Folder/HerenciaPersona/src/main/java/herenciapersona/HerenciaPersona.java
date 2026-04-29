6b7b3a63e494b4195d6d4a3733d1ef8d63b9e8c1
demonassit <jminorg@ipn.mx>
2026-04-28 20:51:14 -0600

    crud estudiante queda como tarea aplicar el set y debe de guardar 5 estudiantes deberan de entregar la captura de pantalla de su ejecución y el codigo

---
+++ New Folder/HerenciaPersona/src/main/java/herenciapersona/HerenciaPersona.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package herenciapersona;
+
+/**
+ *
+ * @author demon
+ */
+public class HerenciaPersona {
+
+    public static void main(String[] args) {
+        DAOEstudiante objmenu = new DAOEstudiante();
+        objmenu.menu();
+    }
+}
